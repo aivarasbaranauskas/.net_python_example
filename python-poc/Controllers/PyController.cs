d04d9e5af864344b9d71bda35d2a2d96855f347e
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2021-06-11 09:18:03 +0300

    init

---
+++ python-poc/Controllers/PyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace python_poc.Controllers
+{
+    [ApiController]
+    [Route("py")]
+    public class PyController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "hello?";
+        }
+    }
+}
